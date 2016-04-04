b92570ff4901eef889b32b2039eed651d70301c3
xwq520 <250504235@qq.com>
2016-04-05 00:31:51 +0800

    1:Volley框架的整合

--- app/src/main/java/com/example/com/applicationdemo/MainActivity.java
+++ app/src/main/java/com/example/com/applicationdemo/MainActivity.java
@@ -2,19 +2,37 @@ package com.example.com.applicationdemo;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    // 生产发送http网络请求的 请求队列
+    private RequestQueue requestQueue = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +41,17 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // 生产发送http网络请求的 请求队列
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        Toast.makeText(getApplicationContext(), requestQueue.toString() + "==Obj", Toast.LENGTH_LONG).show();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
 
@@ -81,8 +104,53 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
+            StringBuilder httpurl = new StringBuilder("http://www.weather.com.cn/data/sk/101210101.html");
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("name1", "value1");
+            map.put("name2", "value2");
+            JSONObject jsonObject = new JSONObject(map);
+            JsonRequest<JSONObject> jsonRequest = new JsonObjectRequest(httpurl.toString(), null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.d("TAG", response.toString());
+                            Toast.makeText(getApplicationContext(), response.toString()+"==OK", Toast.LENGTH_LONG).show();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), "VolleyError", Toast.LENGTH_LONG).show();
+                }
+            });
+            requestQueue.add(jsonRequest);
+
+
+           /* {
+                //注意此处override的getParams()方法,在此处设置post需要提交的参数根本不起作用
+                //必须象上面那样,构成JSONObject当做实参传入JsonObjectRequest对象里
+                //所以这个方法在此处是不需要的
+//    @Override
+//    protected Map<String, String> getParams() {
+//          Map<String, String> map = new HashMap<String, String>();
+//            map.put("name1", "value1");
+//            map.put("name2", "value2");
+
+//        return params;
+//    }
+                @Override
+                public Map<String, String> getHeaders() {
+                    HashMap<String, String> headers = new HashMap<String, String>();
+                    headers.put("Accept", "application/json");
+                    headers.put("Content-Type", "application/json; charset=UTF-8");
+
+                    return headers;
+                }
+            };*/
+
+
         } else if (id == R.id.nav_gallery) {
+            Toast.makeText(getApplicationContext(), "nav_gallery", Toast.LENGTH_LONG).show();
 
         } else if (id == R.id.nav_slideshow) {
 
